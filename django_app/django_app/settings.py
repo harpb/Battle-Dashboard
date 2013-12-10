4f2f87143b2eb2afe632b8c7dad3070e0c8216de
Harp. B <hi@harpb.com>
2013-12-09 18:03:06 -0800

    - specified static collection dir

--- django_app/django_app/settings.py
+++ django_app/django_app/settings.py
@@ -102,6 +102,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static_collection")
 
 #===============================================================================
 # SERVER ENVIRONMENT
